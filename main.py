30def9b9ba337d4b11efcf93a9e03dc268ee706b
Jack Cargill <jack.cargill@passfort.com>
2022-02-17 13:58:52 +0000

    Minimal ineffective change to trigger CI workflow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.application import app
 
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
